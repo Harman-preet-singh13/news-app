fbbd38ba70e436a7e9c564da41fbcacd599a963f
Harmanpreet Singh <harmanp397@gmail.com>
2023-10-09 13:39:11 +0530

    api

---
+++ src/app/api/api.js
@@ -0,0 +1,6 @@
+
+// const TopNewsapi =  "https:newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3440d998412d45c88371246b12c8294e"
+
+const apiKey = "3440d998412d45c88371246b12c8294e";
+
+export default apiKey;
